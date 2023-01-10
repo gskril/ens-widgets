a12531200a77c43ed02d13fb3380091c87faf778
Greg <gskril@gmail.com>
2023-01-10 23:41:08 +0200

    Clean package.json and fix Rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,8 @@
-import { terser } from '@rollup/plugin-terser'
 import commonjs from '@rollup/plugin-commonjs'
 import dts from 'rollup-plugin-dts'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 
 import packageJson from './package.json' assert { type: 'json' }
