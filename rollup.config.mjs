14cd9383d4a7150c11162ac429792319204d1674
Greg <gskril@gmail.com>
2023-01-13 14:44:08 +0200

    Output ESM only
    When outputting ES and CJS modules, Next.js imports the wrong one for some reason. Ideally I can find a better solution but this should be fine for now

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -12,11 +12,6 @@ export default [
     input: 'src/index.ts',
     output: [
       {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
         file: packageJson.module,
         format: 'esm',
         sourcemap: true,
