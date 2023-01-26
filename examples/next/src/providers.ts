27466661ddd60fa43383f63166703dd049ca42f2
Greg <gskril@gmail.com>
2023-01-26 23:32:32 +0200

    Use ethers publicProvider in example

--- examples/next/src/providers.ts
+++ examples/next/src/providers.ts
@@ -1,14 +1,11 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createClient, WagmiConfig } from 'wagmi'
 import { mainnet, goerli } from 'wagmi/chains'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
-
-const ALCHEMY_ID = process.env.NEXT_PUBLIC_ALCHEMY_ID || ''
 
 export const { chains, provider } = configureChains(
   [mainnet, goerli],
-  [alchemyProvider({ apiKey: ALCHEMY_ID }), publicProvider()]
+  [publicProvider()]
 )
 
 const { connectors } = getDefaultWallets({
