d17bac6bf31b987aed7a8201c5317c6c35c0eecd
Greg <gskril@gmail.com>
2023-02-01 22:48:53 +0200

    Build widget to /dist root

--- packages/widget/rollup.config.mjs
+++ packages/widget/rollup.config.mjs
@@ -26,7 +26,7 @@ export default [
     ],
   },
   {
-    input: 'dist/esm/types/index.d.ts',
+    input: 'dist/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
